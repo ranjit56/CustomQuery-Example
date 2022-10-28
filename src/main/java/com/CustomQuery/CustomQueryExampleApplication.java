c8aa5373c4c8e9eef5a5b5d70d56f6b87d696d44
Ranjith Indluru <ranjith.indluru@ratnaglobaltech.com>
2022-10-28 10:25:01 +0530

    second commit

--- src/main/java/com/CustomQuery/CustomQueryExampleApplication.java
+++ src/main/java/com/CustomQuery/CustomQueryExampleApplication.java
@@ -1,12 +1,17 @@
 package com.CustomQuery;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CustomQueryExampleApplication {
 
+	public final static Logger logger = LoggerFactory.getLogger(CustomQueryExampleApplication.class);
+
 	public static void main(String[] args) {
+		logger.info("Integration");
 		SpringApplication.run(CustomQueryExampleApplication.class, args);
 	}
 
--- src/test/java/com/CustomQuery/CustomQueryExampleApplicationTests.java
+++ src/test/java/com/CustomQuery/CustomQueryExampleApplicationTests.java
@@ -1,13 +1,18 @@
 package com.CustomQuery;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
+
 
 @SpringBootTest
 class CustomQueryExampleApplicationTests {
 
+	public final static Logger logger = LoggerFactory.getLogger(CustomQueryExampleApplicationTests.class);
 	@Test
 	void contextLoads() {
+		logger.info("Test");
 	}
 
 }
